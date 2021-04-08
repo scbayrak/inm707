67495570823e7138adc9998b5dd4cf43b61e2705
scbayrak <63463366+scbayrak@users.noreply.github.com>
2021-04-09 00:32:05 +0300

    Task 3 Linting

--- Task_3/experiment.py
+++ Task_3/experiment.py
@@ -1,74 +1,87 @@
-import gym
-from model import PPO
-from PPO_learning import PpoLearning
+""" Conducts training and evaluation per test cases in test.yaml.
+
+"""
+
 import yaml
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import numpy as np
-
+import gym
+from model import PPO
+from PPO_learning import PpoLearning
 
 FILENAME = "tests.yaml"
 
 def plot_graphs(test_dict, rewards):
+    """ Forward propogation.
 
-        gs = gridspec.GridSpec(1, 4, wspace=0.3)
-        fig = plt.figure(figsize=(20, 6))
-        fig.suptitle('Experiment Results', y=0.93)
+        Args:
+            test_dict: Dictionary with hyper-parameters for the test case.
 
-        x = np.arange(1, len(rewards)+1)
+        Returns:
+            None
 
-        ax1 = plt.subplot(gs[:, 1:])
-        ax1.scatter(x, rewards, s=5)
-        ax1.set_title('Rewards')
-        ax1.set(xlabel='Game', ylabel='Reward')
-        
-        ax2 = plt.subplot(gs[:, :1])
-        ax2.axis('off')
-        data = [[key, val] for key, val in test_dict.items()]
-        ax2.table(cellText = data, cellLoc='center', bbox = [0,0,1,1])
+    """
+    g_s = gridspec.GridSpec(1, 4, wspace=0.3)
+    fig = plt.figure(figsize=(20, 6))
+    fig.suptitle('Experiment Results', y=0.93)
 
-        plt.savefig(f'results/charts/Test_{test_dict["test_no"]}.png', bbox_inches='tight')
+    x_values = np.arange(1, len(rewards)+1)
+
+    ax1 = plt.subplot(g_s[:, 1:])
+    ax1.scatter(x_values, rewards, s=5)
+    ax1.set_title('Rewards')
+    ax1.set(xlabel='Game', ylabel='Reward')
+
+    ax2 = plt.subplot(g_s[:, :1])
+    ax2.axis('off')
+    data = [[key, val] for key, val in test_dict.items()]
+    ax2.table(cellText = data, cellLoc='center', bbox = [0,0,1,1])
+
+    plt.savefig(f'results/charts/Test_{test_dict["test_no"]}.png', bbox_inches='tight')
 
 def run_tests():
-    
+    """ Conducts training, evaluation, creates results table and plots.
+
+        Returns:
+            results: Results dataframe
+
+    """
+
     with open(FILENAME) as file:
 
-                # The FullLoader parameter handles the conversion from YAML
-                # scalar values to Python the dictionary format
-                tests = yaml.safe_load(file)
+        # Loads the test hyper-parameters as dictionaries.
+        tests = yaml.safe_load(file)
 
-
-    # create a dataframe to keep the results 
+    # create a dataframe to keep the results
     test_dict = tests['Tests']
-    df = pd.DataFrame(test_dict)
-    df["Episode"] = ""
-    df['Max average score'] = ""
-    df['Learning Iterations'] = ""
+    results = pd.DataFrame(test_dict)
+    results["Episode"] = ""
+    results['Max average score'] = ""
+    results['Learning Iterations'] = ""
 
     # run experiments:
     for i, test in enumerate(tests['Tests']):
-        
+
         env = gym.make(test['env'])
         env.reset()
 
-        ppo = PPO(env, test['T'], test['hidden_dim'], test['gamma'], 
+        ppo = PPO(env, test['T'], test['hidden_dim'], test['gamma'],
         test['gae_lambda'], test['clip'], test['no_batches'],
         test['epochs'], test['lr_actor'], test['lr_critic'])
 
         training = PpoLearning(test['no_games'], ppo, test['max_score'], test['save'], test['test_no'])
         avg_score, game, iters, rewards = training.train()
 
-        df.loc[i,'Episode'] = game
-        df.loc[i,'Max average score'] = avg_score
-        df.loc[i,'Learning Iterations'] = iters
+        results.loc[i,'Episode'] = game
+        results.loc[i,'Max average score'] = avg_score
+        results.loc[i,'Learning Iterations'] = iters
 
         plot_graphs(test, rewards)
 
-    # save to csv file
+    # save results to csv file
     filename = 'results/' + 'test_table.csv'
-    df.to_csv(filename)
-    return df
-
-run_tests()
+    results.to_csv(filename)
+    return results
 
--- Task_3/model.py
+++ Task_3/model.py
@@ -1,18 +1,31 @@
+""" PPO model to solve the environment.
+
+"""
+
 import os
-import numpy as np
-from memory import Memory
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.distributions.categorical import Categorical
+from memory import Memory
 
-os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+os.environ['KMP_DUPLICATE_LIB_OK']='True' # required to run on MAC OS
+
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-class Actor(nn.Module):
 
+class Actor(nn.Module):
+    """ Actor Neural Network class.
+
+    Args:
+        input_dim: Network input dimension.
+        hidden_dim: Hidden layer dimension.
+        output_dim: Network output dimension.
+
+    """
     def __init__(self, input_dim, hidden_dim, output_dim):
 
         super().__init__()
@@ -20,9 +33,17 @@ class Actor(nn.Module):
         self.fc2 = nn.Linear(hidden_dim, hidden_dim)
         self.fc3 = nn.Linear(hidden_dim, hidden_dim)
         self.fc4 = nn.Linear(hidden_dim, output_dim)
-            
+
     def forward(self, X):
-        
+        """ Forward propogation.
+
+        Args:
+            X: Input.
+
+        Returns:
+            Output: Softmax distribution.
+
+        """
         h1 = F.relu(self.fc1(X))
         h2 = F.relu(self.fc2(h1))
         h3 = F.relu(self.fc3(h2))
@@ -32,7 +53,13 @@ class Actor(nn.Module):
 
 
 class Critic(nn.Module):
+    """ Critic Neural Network class.
 
+    Args:
+        input_dim: Network input dimension.
+        hidden_dim: Hidden layer dimension.
+
+    """
     def __init__(self, input_dim, hidden_dim):
 
         super().__init__()
@@ -40,9 +67,17 @@ class Critic(nn.Module):
         self.fc2 = nn.Linear(hidden_dim, hidden_dim)
         self.fc3 = nn.Linear(hidden_dim, hidden_dim)
         self.fc4 = nn.Linear(hidden_dim, 1)
-            
-    def forward(self, X):
 
+    def forward(self, X):
+        """ Forward propogation.
+
+        Args:
+            X: Input.
+
+        Returns:
+            Output: Softmax distribution.
+
+        """
         h1 = F.relu(self.fc1(X.float()))
         h2 = F.relu(self.fc2(h1))
         h3 = F.relu(self.fc3(h2))
@@ -51,57 +86,103 @@ class Critic(nn.Module):
         return output
 
 class PPO():
+    """ PPO algorithm class with the learning algorithm.
 
+        Args:
+            env: Environment.
+            T: Steps to put into memory for every optimization.
+            hidden_dim: Networks hidden layer neuron numbers
+            gamma: Discount factor for future returns
+            gae_lambda: GAE smoothing parameter
+            clip: PPO actor loss epsilon clipping parameter
+            no_batches: No of batches to split items in memory
+            epochs: Number of epochs per optimization
+            lr_actor: Actor network learning rate
+            lr_critic: Critic network learning rate
+
+        Attributes:
+            state_dim: Dimensions of the environment observation.
+            actions_dim: Dimensions of the action.
+            actor: The actor network.
+            critic: The critic network.
+            actor_optim: Actor network optimizer.
+            critic_optim: Critic network optimizer.
+            memory: Memory object to keep items for optimization
+            env: Environment.
+            gamma: Discount factor for future returns
+            gae_lambda: GAE smoothing parameter
+            clip: PPO actor loss epsilon clipping parameter
+            epochs: Number of epochs per optimization
+
+    """
     def __init__(self, env, T, hidden_dim= 256, gamma=0.99, gae_lambda=0.95,
                 clip=0.2, no_batches=5, epochs=5, lr_actor=0.0001, lr_critic=0.0003):
-                
-                self.env = env
-                self.state_dim = env.observation_space.shape[0]
-                self.actions_dim = env.action_space.n
-                self.gamma = gamma
-                self.clip = clip
-                self.epochs = epochs
-                self.gae_lambda = gae_lambda
 
-                self.actor = Actor(self.state_dim, hidden_dim, self.actions_dim)
-                self.actor.to(device)
-                self.critic = Critic(self.state_dim, hidden_dim)
-                self.critic.to(device)
+        self.env = env
+        self.state_dim = env.observation_space.shape[0]
+        self.actions_dim = env.action_space.n
+        self.gamma = gamma
+        self.clip = clip
+        self.epochs = epochs
+        self.gae_lambda = gae_lambda
 
-                self.actor_optim = optim.Adam(self.actor.parameters(), lr=lr_actor)
-                self.critic_optim = optim.Adam(self.critic.parameters(), lr=lr_critic)
+        self.actor = Actor(self.state_dim, hidden_dim, self.actions_dim)
+        self.actor.to(device)
+        self.critic = Critic(self.state_dim, hidden_dim)
+        self.critic.to(device)
 
-                self.memory = Memory(no_batches, T)
+        self.actor_optim = optim.Adam(self.actor.parameters(), lr=lr_actor)
+        self.critic_optim = optim.Adam(self.critic.parameters(), lr=lr_critic)
+
+        self.memory = Memory(no_batches, T)
 
     def save_weights(self):
-        
+        """ Saves network weights for maximum scores.
+
+        """
         torch.save(self.actor.state_dict(), "model_weights/actor.pth")
         torch.save(self.critic.state_dict(), "model_weights/critic.pth")
-    
-    def get_action_value(self, state):
 
+    def get_action_value(self, state):
+        """ Gets action, probability and value from the state
+
+        Args:
+            state: Current state.
+
+        Returns:
+            action: Chosen action for the agent
+            log_prob: Log-probability of the chosen action.
+            state_value: The value of the state estimated by critic network.
+
+        """
         state = torch.tensor([state], dtype=torch.float).to(device)
 
         self.actor.eval()
         self.critic.eval()
 
         distribution = Categorical(self.actor(state))
-        action = distribution.sample() 
+        action = distribution.sample()
         log_prob = torch.squeeze(distribution.log_prob(action)).item()
         action = torch.squeeze(action).item()
 
-        state_value = self.critic(state)  
+        state_value = self.critic(state)
         state_value = torch.squeeze(state_value).item()
-        
+
         return action, log_prob, state_value
 
     def optimize(self):
+        """ Optimizes the actor and critic networks.
 
-        for epoch in range(self.epochs):
-                    
+        Returns:
+            None
+
+        """
+
+        for _ in range(self.epochs):
+
             batch_items = self.memory.get_batches()
             states, values, actions, log_probs,\
-            rewards, dones = batch_items[0] 
+            rewards, dones = batch_items[0]
             batch_inds = batch_items[1]
 
             # calculate the Generalized Advantage Estimation
@@ -113,11 +194,10 @@ class PPO():
                 # add the discounted deltas of the next steps
                 gae = delta + self.gamma * self.gae_lambda * gae * (1-int(dones[i]))
                 advantage.insert(0, gae)
-    
+
             # normalize advantage
             advantage = torch.tensor(advantage).to(device)
-            # advantage = (advantage - advantage.mean()) / (advantage.std() + 1e-8)
-            # The simpler form of normalization below 
+            # The simpler form of normalization below gives better results
             advantage = advantage / 10
 
             values = torch.tensor(values).to(device)
@@ -126,29 +206,30 @@ class PPO():
                 states_batch = torch.tensor(states[batch], dtype=torch.float).to(device)
                 old_log_probs = torch.tensor(log_probs[batch]).to(device)
                 actions_batch = torch.tensor(actions[batch]).to(device)
-                
+
+                # when values in memory are used, pytorch gives errors during backprop.
                 self.critic.eval()
                 critic_values = self.critic(states_batch)
                 critic_values = torch.squeeze(critic_values)
                 self.critic.train()
 
+                # get the new probability from the latest policy
                 self.actor.eval()
                 new_distribution = Categorical(self.actor(states_batch))
                 new_log_probs = new_distribution.log_prob(actions_batch)
                 self.actor.train()
 
                 ratio = (new_log_probs - old_log_probs).exp()
-           
+
                 surrogate = advantage[batch] * ratio
                 clipped_surrogate = torch.clamp(ratio, 1-self.clip,
                                     1+self.clip) * advantage[batch]
 
                 actor_loss = -torch.min(surrogate, clipped_surrogate).mean()
-                # print(f"Actor loss {actor_loss}")
 
                 # advantage = returns - values
                 returns = (advantage[batch] + values[batch]).float()
-     
+
                 loss_funct = nn.MSELoss()
                 critic_loss = loss_funct(returns, critic_values)
 
@@ -159,4 +240,5 @@ class PPO():
                 self.critic_optim.zero_grad()
                 critic_loss.backward()
                 self.critic_optim.step()
+
 
